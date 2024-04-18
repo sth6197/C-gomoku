38ec93e2b4274d17d66e7c7174d37e605f35683c
sth6197 <sth6197@gmail.com>
2024-04-19 02:16:44 +0900

    오목 만들기2

--- 오목만들기/오목만들기/오목.c
+++ 오목만들기/오목만들기/오목.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
 
+#define BOARD 19
+#define BLACK_STONE "¡Û"
+#define WHITE_STONE "¡Ü"
 
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+#define SPACE 32
+
+void gotoxy(int x, int y)
+{
+	COORD pos = { x - 1, y - 1 };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+ 
 int main()
 {
 
